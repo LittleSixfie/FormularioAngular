4e340a32bfbed542e9ef99ca7b486f2f9372fcbd
Jorge <jorge-padron99@hotmail.com>
2022-04-17 20:46:06 +0200

    Second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Mascota } from './form.model';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Formulario';
+  mascotaModel = new Mascota("Ejemplo", "Bulldog", 5);
+  empList: Array<Mascota> = [];
+  
+  constructor() {}
+
+  ngOnInit() {}
+
+  submitted = false;
+
+  onSubmit() {
+    this.submitted = true;
+  }
+
+  newMascot() {
+    this.empList.push(
+      new Mascota(
+        this.mascotaModel.nombre,
+        this.mascotaModel.raza,
+        this.mascotaModel.edad
+      )
+    );
+    console.log(this.mascotaModel);
+  }
+  getMascotsByID(name: String) {
+    for(let i =0; i < this.empList.length; i++){
+      if(name == this.empList[i].nombre){
+        this.mascotaModel = this.empList[i];
+      }
+    }
+    
+   
+  }
+
+  
+
+  removeUser(name: String) {
+    this.empList = this.empList.filter((x) => x.nombre != name);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatListModule } from '@angular/material/list';  
+import {MatFormFieldModule} from '@angular/material/form-field';   
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatListModule,
+    MatFormFieldModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
